d6c205dce2c084f8d033de15b3e01e6431d23127
Hassan Ezz <hassanezz280@gmail.com>
2022-11-28 13:57:45 -0800

    First commit

---
+++ CalcApiAppCodeInsideDevOps/Controllers/ValuesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using HassanCalcDev;
+using System.Linq;
+
+//https://github.com/HassanEzz1/ApiApp2.git
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CalcApiAppCodeInsideDevOps.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        Calc x =new Calc();
+
+        // GET: api/<ValuesController>
+        [HttpGet("/average")]
+        public int Average([FromQueryAttribute]int[]nums)
+        {
+               return x.Average(nums);
+
+        }
+
+
+        [HttpGet("/smallest")]
+        public int Smallest([FromQueryAttribute] int[] nums)
+        {
+          return  x.Smallest(nums);
+        }
+
+
+        [HttpGet("/largest")]
+        public int Largest([FromQueryAttribute] int[] nums)
+        {
+            return x.Largest(nums);
+        }
+    }
+
+        
+    
+}
